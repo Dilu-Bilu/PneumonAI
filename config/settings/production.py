283af3d408924cae900679e4bec5f79d0d70216f
Dilu-Bilu <dilreetraju@gmail.com>
2022-07-21 20:03:41 -0700

    changed caching

--- config/settings/production.py
+++ config/settings/production.py
@@ -17,17 +17,12 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # no
 # CACHES
 # ------------------------------------------------------------------------------
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env("REDIS_URL"),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            # Mimicing memcache behavior.
-            # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
-            "IGNORE_EXCEPTIONS": True,
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
         },
     }
-}
+
 
 # SECURITY
 # ------------------------------------------------------------------------------
